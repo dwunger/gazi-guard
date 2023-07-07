ac4770b0300fa9f33d927eaf808fda69483a9e13
dwunger <106967706+dwunger@users.noreply.github.com>
2023-07-07 19:48:55 -0400

    adding logic to reopen unpacked archives

--- pak_merge_helper.py
+++ pak_merge_helper.py
@@ -2,6 +2,45 @@ import zipfile
 import os
 import sys 
 import subprocess
+import filecmp
+import shutil
+
+def are_dirs_identical(dir1, dir2):
+    # Compare files in the directories
+    comparison = filecmp.dircmp(dir1, dir2)
+
+    # Directories are not the same if there are any files that differ
+    if comparison.diff_files or comparison.left_only or comparison.right_only:
+        return False
+
+    # Recurse into subdirectories
+    for subdir in comparison.common_dirs:
+        if not are_dirs_identical(os.path.join(dir1, subdir), os.path.join(dir2, subdir)):
+            return False
+
+    # No differences found
+    return True
+
+def directory_matches_zip(dir_path, zip_file):
+    """
+    Check if a directory has the same files as a ZipFile.
+    """
+    dir_files = set(os.listdir(dir_path))
+    zip_files = set(zip_file.namelist())
+
+    return dir_files == zip_files
+def directory_matches_zip(dir_path, zip_file):
+    """
+    Check if a directory has the same files as a ZipFile.
+    """
+    # Get a list of all files in dir_path
+    dir_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(dir_path) for f in filenames]
+    dir_files = [file.replace(dir_path + '/', '') for file in dir_files]  # Make paths relative to dir_path
+
+    # Get a list of all files in zip_file
+    zip_files = [name for name in zip_file.namelist() if not name.endswith('/')]  # Remove directories
+
+    return set(dir_files) == set(zip_files)
 
 def increment_path(base_path):
     if base_path not in os.listdir():
@@ -43,7 +82,7 @@ def extract_source_scripts(source_archive, mod_file_names, destination):
         for file in mod_file_names:
             if file in table:
                 zip_ref.extract(file, path=destination)
-                 
+
 def get_mod_files(mod_pak):
     with zipfile.ZipFile(mod_pak, 'r') as mod_zip:
         return mod_zip.namelist()
@@ -52,15 +91,19 @@ def verify_source_paks_exist(source_pak_0, source_pak_1, error_message):
     if not os.path.exists(source_pak_0) or not os.path.exists(source_pak_1):
         raise FileNotFoundError(error_message)
 
+def file_basename(filename):
+    return filename.rsplit('.')[0]
+
 def main():
     # Define the paths to source .pak files
     source_pak_0 = 'data0.pak'
     source_pak_1 = 'data1.pak'
     mod_pak = choose_mod_pak('mod.pak')
-    
-    mod_unpack_path = increment_path(f"{mod_pak}_mod_scripts")
-    merged_unpack_path = increment_path(f'{mod_pak}_source_scripts')
 
+    mod_unpack_path = f"{file_basename(mod_pak)}_mod_scripts"
+    merged_unpack_path = f'{file_basename(mod_pak)}_source_scripts'
+
+    # Check source pak files
     file_missing_error = "\nOne or both source pak files are missing (data0.pak and/or data1.pak)." \
                         " Try running from ./steamapps/common/Dying Light 2/ph/source/"
       
@@ -69,13 +112,34 @@ def main():
     # Open the mod.pak file and get the list of file names
     mod_file_names = get_mod_files(mod_pak)
 
-    # Unzip relevant scripts from source_pak_0 and source_pak_1 to ./source_scripts/                
+    # Unzip relevant scripts from source_pak_0 and source_pak_1 to ./{data_n}_source_scripts/                
     extract_source_scripts(source_pak_0, mod_file_names, merged_unpack_path)
     extract_source_scripts(source_pak_1, mod_file_names, merged_unpack_path)
 
-    # Unzip mod.pak to mod_scripts/
+    # Open mod.pak without unzipping
+    # Inside the main function...
+    # Open mod.pak without unzipping
     with zipfile.ZipFile(mod_pak, 'r') as mod_zip:
-        mod_zip.extractall(mod_unpack_path)
+        # If the target directory exists and matches the contents of the .pak file,
+        # then there's no need to unzip again
+        if os.path.exists(mod_unpack_path) and directory_matches_zip(mod_unpack_path, mod_zip):
+            print("The mod scripts are already extracted and up-to-date.")
+            identical_check = input("Do you want to check for identical content? (y/n) ")
+            if identical_check.lower() == "y":
+                # Here you can add the logic to compare the content of the files in the directory and the .pak
+            else:
+                mod_zip.extractall('temp')
+                if
+        else:
+            # Prompt to overwrite or increment path
+            overwrite = input(f"{mod_unpack_path} already exists. Overwrite? (y/n) ")
+            if overwrite.lower() != "y":
+                print("Exiting without overwriting.")
+                return
+            if os.path.exists(mod_unpack_path):
+                shutil.rmtree(mod_unpack_path)  # Remove existing directory
+            # Unzip mod.pak to mod_unpack_path
+            mod_zip.extractall(mod_unpack_path)
 
     print(f"\n\nComparison complete! \n\nSee for output:\nUnpacked mod scripts → ./{mod_unpack_path}\nUnpacked source scripts → ./{merged_unpack_path}\n")
 
