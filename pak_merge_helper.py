64c2791a0c088a448c3186819a88aaa1e349c9cd
dwunger <106967706+dwunger@users.noreply.github.com>
2023-07-07 17:05:32 -0400

    Updated for modularity

--- pak_merge_helper.py
+++ pak_merge_helper.py
@@ -25,54 +25,63 @@ def progressbar(it, prefix="", size=60, out=sys.stdout):
         show(i+1)
     print("\n", flush=True, file=out)
     
-# Define the paths to your source .pak files
-source_pak_0 = 'data0.pak'
-source_pak_1 = 'data1.pak'
+def extract_source_scripts(source_archive, mod_file_names, destination):
+    '''args: source archive path, list of candidate files to extract, extract to path'''
+    #pass source_pak_0 and source_pak_1
+    with zipfile.ZipFile(source_archive, 'r') as zip_ref:
+        table = set(zip_ref.namelist())
+        for file in mod_file_names:
+            if file in table:
+                zip_ref.extract(file, path=destination)
+                 
+def get_mod_files(mod_pak):
+    with zipfile.ZipFile(mod_pak, 'r') as mod_zip:
+        return mod_zip.namelist()
 
-# Define the path to your mod .pak file
-mod_pak = 'mod.pak' # previously data4.pak
-mod_unpack_path = increment_path("mod_scripts")
-# Define the path for the new pak file that contains merged content
-merged_unpack_path = increment_path('source_scripts')
-merged_pak = merged_unpack_path[0:-1] + '.pak' # -> source_scripts.pak
+def verify_source_paks_exist(source_pak_0, source_pak_1, error_message):
+    if not os.path.exists(source_pak_0) or not os.path.exists(source_pak_1):
+        raise FileNotFoundError(error_message)
 
-# Define the list of files in mod.pak
-mod_files = []
-if not os.path.exists(source_pak_0) or not os.path.exists(source_pak_1):
-    raise FileNotFoundError("\n\nOne or both source pak files are missing (data0.pak and/or data1.pak). Try running from ./steamapps/common/Dying Light 2/ph/source/")
-                            
-if not os.path.exists(mod_pak):
-    for idx, file in enumerate(os.listdir()):
-        print(f'{idx} : {file}')
-    mod_idx = input("Enter mod archive index: ")
-    mod_pak = os.listdir()[int(mod_idx)]
+def main():
+    # Define the paths to source .pak files
+    source_pak_0 = 'data0.pak'
+    source_pak_1 = 'data1.pak'
+    mod_pak = 'mod.pak'
+    mod_unpack_path = increment_path("mod_scripts")
+    merged_unpack_path = increment_path('source_scripts')
 
-# Open the mod.pak file and get the list of file names
-with zipfile.ZipFile(mod_pak, 'r') as mod_zip:
-    mod_files = mod_zip.namelist()
+    file_missing_error = "\nOne or both source pak files are missing (data0.pak and/or data1.pak)." \
+                        " Try running from ./steamapps/common/Dying Light 2/ph/source/"
+      
+    verify_source_paks_exist(source_pak_0, source_pak_1, file_missing_error)              
+    
+    if not os.path.exists(mod_pak):
+        dir_list = [f for f in os.listdir() if f.endswith('.pak')]
+        for idx, file in enumerate(dir_list):
+            print(f'{idx} : {file}')
+        mod_idx = input("Enter mod archive index: ")
+        mod_pak = dir_list[int(mod_idx)]
 
-# Unzip relevant scripts from source_pak_0 and source_pak_1 to ./source_scripts/
-with zipfile.ZipFile(source_pak_0, 'r') as zip_ref:
-    table = set(zip_ref.namelist())
-    for file in mod_files:
-        if file in table:
-            zip_ref.extract(file, path=merged_unpack_path)
+    # Open the mod.pak file and get the list of file names
+    mod_file_names = get_mod_files(mod_pak)
 
-with zipfile.ZipFile(source_pak_1, 'r') as zip_ref:
-    table = set(zip_ref.namelist())
-    for file in mod_files:
-        if file in table:
-            zip_ref.extract(file, path=merged_unpack_path)
+    # Unzip relevant scripts from source_pak_0 and source_pak_1 to ./source_scripts/                
+    extract_source_scripts(source_pak_0, mod_file_names, merged_unpack_path)
+    extract_source_scripts(source_pak_1, mod_file_names, merged_unpack_path)
 
-# Unzip mod.pak to mod_scripts/
-with zipfile.ZipFile(mod_pak, 'r') as mod_zip:
-    mod_zip.extractall(mod_unpack_path)
+    # Unzip mod.pak to mod_scripts/
+    with zipfile.ZipFile(mod_pak, 'r') as mod_zip:
+        mod_zip.extractall(mod_unpack_path)
 
-print(f"\n\nComparison complete! \n\nSee for output:\nUnpacked mod scripts → ./{mod_unpack_path}\nUnpacked source scripts → ./{merged_unpack_path}\n")
+    print(f"\n\nComparison complete! \n\nSee for output:\nUnpacked mod scripts → ./{mod_unpack_path}\nUnpacked source scripts → ./{merged_unpack_path}\n")
 
-try:
-    # Start 2-way comparison with Meld using subprocess.Popen
-    subprocess.Popen(['meld', mod_unpack_path, merged_unpack_path])
-    print("Launching Meld for review...")
-except FileNotFoundError:
-    print('\nMeld does not appear in PATH. Please install from https://meld.app/ or get the source code \nfrom https://gitlab.gnome.org/GNOME/meld and add to user/system PATH to initiate comparison directly.\n')
+    try:
+        # Start 2-way comparison with Meld using subprocess.Popen
+        subprocess.Popen(['meld', mod_unpack_path, merged_unpack_path])
+        print("Launching Meld for review...")
+    except FileNotFoundError:
+        print('\nMeld does not appear in PATH. Please install from https://meld.app/ or get the source code "\
+            "\nfrom https://gitlab.gnome.org/GNOME/meld and add to user/system PATH to initiate comparison directly.\n')
+
+if __name__ == '__main__':
+    main()
