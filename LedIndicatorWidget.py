5e93c26dc4c79c4bcb211161d329e74dbe016304
dwunger <106967706+dwunger@users.noreply.github.com>
2023-07-18 09:03:51 -0400

    LED widget for sync status

---
+++ LedIndicatorWidget.py
@@ -0,0 +1,94 @@
+from PyQt5.QtCore import Qt, QPointF, pyqtProperty
+from PyQt5.QtGui import QColor, QPainter, QPen, QBrush, QRadialGradient
+from PyQt5.QtWidgets import QAbstractButton
+
+
+
+class LedIndicator(QAbstractButton):
+    scaledSize = 1000.0
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setMinimumSize(14, 14)
+        self.setCheckable(True)
+        self.on_color_1 = QColor(0, 255, 0)
+        self.on_color_2 = QColor(0, 192, 0)
+        self.off_color_1 = QColor(255, 0, 0)
+        self.off_color_2 = QColor(192, 0, 0)
+
+    def resizeEvent(self, event):
+        self.update()
+
+    def paintEvent(self, event):
+        realSize = min(self.width(), self.height())
+
+        painter = QPainter(self)
+        pen = QPen(Qt.black)
+        pen.setWidth(1)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.translate(self.width() / 2, self.height() / 2)
+        painter.scale(realSize / self.scaledSize, realSize / self.scaledSize)
+
+        self.drawBackground(painter, pen)
+        self.drawIndicator(painter, pen)
+
+    def drawBackground(self, painter, pen):
+        gradient = QRadialGradient(QPointF(-500, -500), 1500, QPointF(-500, -500))
+        gradient.setColorAt(0, QColor(224, 224, 224))
+        gradient.setColorAt(1, QColor(28, 28, 28))
+        painter.setPen(pen)
+        painter.setBrush(QBrush(gradient))
+        painter.drawEllipse(QPointF(0, 0), 500, 500)
+
+        gradient = QRadialGradient(QPointF(500, 500), 1500, QPointF(500, 500))
+        gradient.setColorAt(0, QColor(224, 224, 224))
+        gradient.setColorAt(1, QColor(28, 28, 28))
+        painter.setPen(pen)
+        painter.setBrush(QBrush(gradient))
+        painter.drawEllipse(QPointF(0, 0), 450, 450)
+
+    def drawIndicator(self, painter, pen):
+        painter.setPen(pen)
+        if self.isChecked():
+            gradient = QRadialGradient(QPointF(-500, -500), 1500, QPointF(-500, -500))
+            gradient.setColorAt(0, self.on_color_1)
+            gradient.setColorAt(1, self.on_color_2)
+        else:
+            gradient = QRadialGradient(QPointF(500, 500), 1500, QPointF(500, 500))
+            gradient.setColorAt(0, self.off_color_1)
+            gradient.setColorAt(1, self.off_color_2)
+
+        painter.setBrush(gradient)
+        painter.drawEllipse(QPointF(0, 0), 400, 400)
+
+    @pyqtProperty(QColor)
+    def onColor1(self):
+        return self.on_color_1
+
+    @onColor1.setter
+    def onColor1(self, color):
+        self.on_color_1 = color
+
+    @pyqtProperty(QColor)
+    def onColor2(self):
+        return self.on_color_2
+
+    @onColor2.setter
+    def onColor2(self, color):
+        self.on_color_2 = color
+
+    @pyqtProperty(QColor)
+    def offColor1(self):
+        return self.off_color_1
+
+    @offColor1.setter
+    def offColor1(self, color):
+        self.off_color_1 = color
+
+    @pyqtProperty(QColor)
+    def offColor2(self):
+        return self.off_color_2
+
+    @offColor2.setter
+    def offColor2(self, color):
+        self.off_color_2 = color
