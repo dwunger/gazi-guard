c9e6468929b2232253a140db5131e6ed792f3279
dwunger <106967706+dwunger@users.noreply.github.com>
2023-07-18 21:58:16 -0400

    toast and rate limiter grouped to module

---
+++ notifs.py
@@ -0,0 +1,20 @@
+from win10toast import ToastNotifier
+from plyer import notification
+import time
+class RateLimitedNotifier:
+    def __init__(self, min_interval=5):  # interval in seconds
+        self.min_interval = min_interval
+        self.last_notify_time = 0
+
+    def notify(self, title, message):
+        current_time = time.time()
+        if current_time - self.last_notify_time > self.min_interval:
+            self.last_notify_time = current_time
+            # Do the notification here
+            notification.notify(title=title, message=message)
+            
+
+
+def show_notification(title, message):
+    toaster = ToastNotifier()
+    toaster.show_toast(title, message, duration=2)
