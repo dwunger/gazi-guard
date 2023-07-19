45a8f733cf5f87c5ae79f32a624c89afd9eb0df8
dwunger <106967706+dwunger@users.noreply.github.com>
2023-07-19 15:17:04 -0400

    new prompt method

--- melder.py
+++ melder.py
@@ -40,6 +40,13 @@ def prompt_enter_config():
         return True
     else:
         return False
+    
+def prompt_delete_backups(message):
+    root = tk.Tk()
+    root.withdraw()
+    response = messagebox.askquestion("Unexpected Backups Found", message, type = messagebox.YESNO)
+    
+    return response
 def launch_meld(meld_path,mod_unpack_path,merged_unpack_path):
     meld_command = meld_path if meld_path else 'meld'
     return subprocess.Popen([meld_command, mod_unpack_path, merged_unpack_path])
