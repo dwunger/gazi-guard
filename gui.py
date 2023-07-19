861c519d6d275b5793898c0851a928001a5ff342
dwunger <106967706+dwunger@users.noreply.github.com>
2023-07-19 05:35:27 -0400

    Comms now build from common message constructor

--- gui.py
+++ gui.py
@@ -2,11 +2,28 @@ import sys
 import threading
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import QProcess
-from LedIndicatorWidget import *
+from led_indicator_widget import LedIndicator
+from abstract_message import AbstractMessage
 import os
 # import DemoAppUi
-
-
+def get_int_date():
+    import datetime
+    current_date = datetime.datetime.now()
+    return current_date.strftime("%Y-%m-%d")
+run_number = get_int_date()
+log_file = f"LOG_{run_number}.txt"
+def logger_iter(iterable):
+    with open(log_file, 'a+') as log:
+        log.write('#######GUI########\n')
+        log.write('iterable: \n')
+        log.writelines(iterable)
+        log.write('\n')
+        log.write('#######GUI########\n')
+def logger_str(text):
+    with open(log_file, 'a+') as log:
+        log.write('#######GUI########\n')
+        log.write(text + "\n")
+        log.write('#######GUI########\n')
 
 class CustomTitleBar(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -128,42 +145,44 @@ class OptionsDialog(QtWidgets.QDialog):
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, backend_script):
         super().__init__()
-        self.backend_script = backend_script
-        self.backend_process = None  # Initialize the backend_process here
 
+        self.backend_script = backend_script
+        self.backend_process = None
+        self.message = AbstractMessage()  # Initialize message here
+        self.pid = os.getpid()
+        self.child_pid = None
+        self.editor_pid = None
+        self.setupUI()
+
+    def setupUI(self):
         self.setWindowTitle("Pak Tools")
         self.resize(400, 200)  # Set the initial size of the window
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # Remove the native window frame
 
-        # Remove the native window frame
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.createTitleBar()
+        self.createToolbar()
+        self.createLEDLayout()
 
-        # Create a custom title bar
+    def createTitleBar(self):
         titleBar = CustomTitleBar(self)
         self.setMenuWidget(titleBar)
 
-        # Create a toolbar
+    def createToolbar(self):
         toolbar = QtWidgets.QToolBar(self)
         toolbar.setMovable(False)  # Disable toolbar movement
         self.addToolBar(toolbar)
 
-        # Create the layout for the LED
-        layout = QtWidgets.QHBoxLayout()
-        self.led = LedIndicator(self)
-        self.led.setDisabled(True)  # Make the led non-clickable
-        layout.addWidget(self.led)
+        # self.createToggleLEDButton(toolbar)
+        self.createFileMenuButton(toolbar)
 
-        central_widget = QtWidgets.QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+    # def createToggleLEDButton(self, toolbar):
+    #     self.pushButton = QtWidgets.QPushButton("Toggle LED", self)
+    #     self.pushButton.clicked.connect(self.onPressButton)
+    #     toolbar.addWidget(self.pushButton)
 
-        self.pushButton = QtWidgets.QPushButton("Toggle LED", self)
-        self.pushButton.clicked.connect(self.onPressButton)
-        toolbar.addWidget(self.pushButton)
-
-        # Create a menu
+    def createFileMenuButton(self, toolbar):
         menu = QtWidgets.QMenu(self)
 
-        # Create actions for the menu
         action1 = QtWidgets.QAction("Open...", self)
         # action1.triggered.connect(self.openFileDialog)
         menu.addAction(action1)
@@ -171,22 +190,46 @@ class MainWindow(QtWidgets.QMainWindow):
         action2 = QtWidgets.QAction("Hide", self)
         menu.addAction(action2)
 
-        # Create actions for the menu
         action3 = QtWidgets.QAction("Options...", self)
         action3.triggered.connect(self.openOptionsDialog)
         menu.addAction(action3)
 
-        # Create a button in the toolbar to display the menu
         menu_button = QtWidgets.QToolButton(toolbar)
         menu_button.setStyleSheet("QToolButton::menu-indicator { image: none; }")  # Remove the menu indicator arrow
         menu_button.setMenu(menu)
         menu_button.setPopupMode(QtWidgets.QToolButton.InstantPopup)
         menu_button.setText("File")
 
-        # Add the button to the toolbar
         toolbar.addWidget(menu_button)
+
+    def createLEDLayout(self):
+        layout = QtWidgets.QHBoxLayout()
+
+        # Vertically center alignment
+        layout.addStretch(1)
+
+        self.led = LedIndicator(self)
+        self.led.setDisabled(True)  # Make the LED non-clickable
+
+        layout.addWidget(self.led)
+
+        # Add space between the LED and the text widget
+        layout.addSpacing(10)
+
+        text_widget = QtWidgets.QLabel("Mod Status")
+        layout.addWidget(text_widget)
+
+        # Vertically center alignment
+        layout.addStretch(1)
+
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+
     def onPressButton(self):
         self.led.setChecked(not self.led.isChecked())
+
         
     def openOptionsDialog(self):
         # Temporarily remove the "Keep on top" flag
@@ -204,17 +247,17 @@ class MainWindow(QtWidgets.QMainWindow):
             self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
             self.show()
             
-    def sendMessageToBackend(self, message):
+    def send_message(self, message):
         if self.backend_process is not None and self.backend_process.state() == QProcess.Running:
             self.backend_process.write(message.encode() + b"\n")
             self.backend_process.waitForBytesWritten()
             
-    def startBackendProcess(self):
+    def init_main_proc(self):
         if self.backend_process is None:
             self.backend_process = QProcess()
             self.backend_process.setProcessChannelMode(QProcess.MergedChannels)
-            self.backend_process.readyRead.connect(self.onReadyRead)
-            self.backend_process.finished.connect(self.onBackendFinished)
+            self.backend_process.readyRead.connect(self.onListenMain)
+            self.backend_process.finished.connect(self.onExitMain)
             self.backend_process.start("python", [self.backend_script])
 
     def stopBackendProcess(self):
@@ -229,37 +272,47 @@ class MainWindow(QtWidgets.QMainWindow):
         print(data)
         lines = data.split('\n')
         messages = [line.lstrip('*') for line in lines if line.startswith('*')]
-        
+        logger_iter(lines)
+        logger_iter(messages)
         return messages
     
-    def onReadyRead(self):
+    def onListenMain(self):
         # Read the data from the stdout and stderr of the process
         raw_data = self.backend_process.readAll().data().decode()
 
         # print(f"Raw data: '{raw_data}'")  # Print raw data
-        messages = self.parse_stream(raw_data)
-        for message in messages:
-            if message == "sync":
-                # Process data
-                self.led.setChecked(True)
-                self.led.update()  # Force the led widget to redraw itself
-            if message == 
-
-
-    def onBackendFinished(self, exit_code, exit_status):
-        # Called when the backend process has finished
+        inbound_messages = self.parse_stream(raw_data)
         
+        for inbound_message in inbound_messages:
+            inbound_message_type, inbound_message = inbound_message.split(':')
+            
+            response = self.get_response(inbound_message_type, inbound_message)
+    def get_response(self, payload_type, payload):
+        #map message type and payload to a response for return value
+        #start with exhaustive switching
+        if payload_type == 'pid':
+            self.child_pid = int(payload)
+            return self.message.pid(os.getpid())
+        if payload_type == 'editor_pid':
+            self.editor_pid = int(payload)            
+    def onExitMain(self, exit_code, exit_status):
+        # Called when the backend process has finished
         print(f"Backend process finished with exit code {exit_code}")
-        os.kill(os.getpid(), 9)
-
+        os.kill(self.pid, 9)
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     
     window = MainWindow("main.py")
     window.show()
-    window.startBackendProcess()  # Start the backend process
+    window.init_main_proc()  # Start the backend process
     # window.sendMessageToBackend
-    window.sendMessageToBackend("state:ready")
-    window.sendMessageToBackend(f"process:{os.getpid}")
+    window.send_message(window.message.request('pid'))
+    # window.send_message(window.message.event('initialized1'))
+    # window.send_message(window.message.event('initialized2'))
+    # window.send_message(window.message.event('initialized3'))
+    # window.send_message(window.message.event('initialized4'))
+    # window.send_message(window.message.event('initialized5'))
+    # window.sendMessageToBackend(f"process:{os.getpid}")
     
     sys.exit(app.exec_())
